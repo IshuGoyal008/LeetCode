3fc9f47db3f5c92b1c84b5f70d50e02b5e500d12
IshuGoyal008 <100957214+IshuGoyal008@users.noreply.github.com>
2023-06-07 01:15:29 +0530

    Create 1793. Maximum Score of a Good Subarray

---
+++ Hard_Leetcode/1793. Maximum Score of a Good Subarray.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int maximumScore(int[] nums, int k) {
+        return Area(nums,k);
+    }
+    public static int Area(int []arr,int k) {
+	Stack<Integer> st=new Stack<>();
+	int ans=0;
+	for (int i = 0; i < arr.length; i++) {
+		
+		while(!st.isEmpty() && arr[i]<arr[st.peek()]) {
+			
+			int r=i;
+			int h=arr[st.pop()];
+			if(r-1>=k) {
+			if(!st.isEmpty()) {
+				int l=st.peek();
+				if(l+1<=k) {
+				ans=Math.max(ans, h*(r-l-1));
+			}
+			}
+			else {
+				
+				ans=Math.max(ans, h*r);
+			}
+		}
+			}
+		st.push(i);
+	}
+	int r=arr.length;
+	while(!st.isEmpty() && r-1>=k) {
+		int h=arr[st.pop()];
+			if(!st.isEmpty()) {
+				int l=st.peek();
+				if(l-1<=k) {
+				ans=Math.max(ans, h*(r-l-1));
+			}
+			}
+			else {
+				
+				ans=Math.max(ans, h*r);
+			}
+		}
+	return ans;
+	}
+}
