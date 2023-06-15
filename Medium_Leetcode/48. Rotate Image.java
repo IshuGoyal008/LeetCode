a5d2d43d8f053e0135df54d28f5388691b5840c1
IshuGoyal008 <100957214+IshuGoyal008@users.noreply.github.com>
2023-06-15 18:08:18 +0530

    Create 48. Rotate Image

---
+++ Medium_Leetcode/48. Rotate Image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i+1;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        for(int i=0;i<matrix.length;i++){
+            int lo=0;
+            int hi=matrix.length-1;
+            while(lo<hi){
+                int temp=matrix[i][lo];
+                matrix[i][lo]=matrix[i][hi];
+                matrix[i][hi]=temp;
+                lo++;
+                hi--;
+            }
+        }
+    }
+}
